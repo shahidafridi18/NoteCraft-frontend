5740b62b2c946e1c25651e1e09fdc2eaa408e0b1
shahidafridi18 <143401661+shahidafridi18@users.noreply.github.com>
2023-12-18 22:52:57 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,58 @@
-import React, { useEffect } from 'react'
-import logo from './notecraft.png'
-import { Link, useLocation,useHistory } from 'react-router-dom';
-
+import React, { useEffect, useState } from 'react';
+import logo from './notecraft.png';
+import { Link, useLocation, useHistory } from 'react-router-dom';
 
 const Navbar = (props) => {
+    const [logoutBtn, setLogoutBtn] = useState(false);
+    const [name, setName] = useState("");
 
     let location = useLocation();
     useEffect(() => {
-        console.log(location.pathname)
+        console.log(location.pathname);
+        const fetchData = async () => {
+            try {
+              const response = await fetch("http://localhost:5000/api/auth/getuser", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                  "auth-token": localStorage.getItem('token')
+                },
+              });
+      
+              if (!response.ok) {
+                console.error('Failed to fetch user details:', response.statusText);
+                return;
+              }
+      
+              const data = await response.json();
+              setName(data.name);
 
-    }, [location])
+            } catch (error) {
+              console.error('Error fetching user details:', error);
+            }
+          };
+      
+          fetchData();
 
+
+    }, [location]);
 
     const isLoggedIn = localStorage.getItem('token');
-    let history=useHistory();
+    let history = useHistory();
 
-    const handleclick=()=>{
+    const handleclick = () => {
         localStorage.removeItem('token');
         history.push("/login");
-        props.showAlert("Successfully logged out!","danger");
-    }
+        props.showAlert("Successfully logged out!", "danger");
+        setLogoutBtn(false);
 
+    }
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/"> <img src={logo} alt="Logo" width="30" height="30" className="d-inline-block align-top" />
-            &nbsp; NoteCraft</Link>
+                    &nbsp; NoteCraft</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -38,22 +64,39 @@ const Navbar = (props) => {
                         <li className="nav-item">
                             <Link className={`nav-link ${location.pathname === '/about' ? "active" : ""}`} to="/about">About</Link>
                         </li>
-
                     </ul>
                     {isLoggedIn ? (
-                        <button className='btn btn-danger' onClick={handleclick}>Logout</button>
+                        <div>
+                        <div>
+                            <button
+                                onClick={() => setLogoutBtn(!logoutBtn)}
+                                className="btn btn-light px-2 text-lg text-dark-800 bg-light py-2 rounded-lg hover:bg-primary hover:text-dark"
+                            >
+                                <i className="fa-solid fa-user"></i>&nbsp; {name}
+                            </button>
+                        </div>
+                        {logoutBtn && (
+                            <div className="position-absolute">
+                                <button
+                                    onClick={handleclick}
+                                    className="btn btn-danger px-3 py-2 text"
+                                >
+                                    Logout
+                                </button>
+                            </div>
+                        )}
+                    </div>
                     ) : (
-                        <form className="d-flex">
-                            <Link className='btn btn-light' role="button" to='/login'>
-                                Login
-                            </Link>
-                            
-                        </form>
-                    )}
+                            <form className="d-flex">
+                                <Link className='btn btn-light' role="button" to='/login'>
+                                    Login
+                                </Link>
+                            </form>
+                        )}
                 </div>
             </div>
         </nav>
     )
 }
 
-export default Navbar
+export default Navbar;
